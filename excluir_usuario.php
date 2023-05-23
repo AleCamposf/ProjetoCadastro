af2a4fbf5f84a7fc0ad118922dd7690c858d8ad7
AleCamposf <124693326+AleCamposf@users.noreply.github.com>
2023-05-23 12:28:03 -0300

    Add files via upload

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -1,19 +1,13 @@
-<?php
-    include("conexao.php");
+<?php 
+include('conexao.php');
+$id_usuario = $_GET['id_usuario']; //Aqui é get, já que passa pela URL
+$sql = "DELETE from usuario WHERE id_usuario=$id_usuario";
 
-    $id = $_GET['id_usuario'];
 
-    $sql = "DELETE FROM usuario WHERE id_usuario='$id'";
-    $result = mysqli_query($con, $sql);
-
+$result = mysqli_query($con,$sql); //a variavel result vai ter o resultado, se deu certo ou n
     if($result){
-        echo "<br><h2>Dados excluidos com sucesso!! </h2>";
-    }else{
-        echo "<br><h2>Erro ao tentar excluir dados: ".$mysqli_error($con). "!";
-    }
-
-    echo
-    "<div>
-    <a href='listar_usuarios.php'>Voltar</a>
-    </div>"; 
+        echo "Dados Alterados com sucesso!<br>";}
+    else{
+        echo "Erro ao alterar dados: ".mysqli_error($con)."<br>";}
 ?>
+<a href="index.php">Voltar</a>
